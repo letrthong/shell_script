5cba8206b7c64676037b84f8040a5c1ae3c064a4
Thong LT <letrthong@gmail.com>
2025-03-28 11:05:10 +0700

    Update main.sh

--- file_system/unittest/main.sh
+++ file_system/unittest/main.sh
@@ -4,25 +4,25 @@ source ./../common_lib.sh
 test_exist_file()
 {
   result=$(exist_file "./main.sh")
-  assertEquals "1" "$result"
+  assertEquals "$ENUM_FILE_EXIST" "$result"
 }
 
 test_exist_file_not()
 {
   result=$(exist_file "./abc.sh")
-  assertEquals "0" "$result"
+  assertEquals "$ENUM_FILE_NOT_EXIST" "$result"
 }
 
 
 test_exist_file_if()
 {
   result=$(exist_file "./main.sh")
-  if [ "$result" -eq 1 ]; then
+  if [ "$result" -eq $ENUM_FILE_EXIST ]; then
     echo "result is equal to 1"
   else
     echo "result is NOT equal to 1"
   fi
-  assertEquals "1" "$result"
+  assertEquals "$ENUM_FILE_EXIST" "$result"
 }
  
  . ./../../shunit2/shunit2
