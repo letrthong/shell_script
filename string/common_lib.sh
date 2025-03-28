464704ae18e659f5ad8eeb3b4d39cfc758baaf9c
Your Name <you@example.com>
2025-03-28 21:25:28 +0700

    clean up

--- string/common_lib.sh
+++ string/common_lib.sh
@@ -11,23 +11,24 @@ func_shared_path()
 # @file_path="/home/thonglt/src/shell_script/file_system/unittest/main.sh"
 # @filter_name "*.sh"
 
-match() {
+match()
+{
     local file_path="${1}";
     local filter_name="${2}";
-
     file_name=$(echo $file_path | grep -o '[^/]*$')
     rexp="${filter_name//\*/.*\\}"
     echo "$file_name" | grep -E "$rexp"
 }
 
-match_with_file() {
+match_with_file()
+{
     local file_path="${1}";
     local filter_name="${2}";
-
     file_name=$(basename "$file_path")
     rexp="${filter_name//\*/.*\\}"
     echo "$file_name" | grep -E "$rexp"
 }
+
 get_length()
 {
   local string="${1}";  # Take the input string as the first argument
@@ -35,7 +36,7 @@ get_length()
 }
 
 count_words()
- {
+{
   local string="$1" # Take the input string as the first argument
   echo "$string" | wc -w # Use wc -w to count the words in the string
 }
