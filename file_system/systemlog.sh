89b8134f539b89b3e3471ca41d26e45bc021e7c6
Thong LT <letrthong@gmail.com>
2025-04-15 09:50:08 +0700

    Create systemlog.sh

---
+++ file_system/systemlog.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+# Your log message
+LOG_MESSAGE="This is a log entry by ThongLT"
+
+# Write the log message to the system log
+logger "$LOG_MESSAGE"
+
+# View the last 10 entries in the system log
+tail -n 10 /var/log/syslog
